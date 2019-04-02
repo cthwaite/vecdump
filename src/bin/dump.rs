aa377f560aa7a6903818fba3d0dc6b5cf5841d01
cthwaite <thwaite@mac.com>
2019-04-02 23:43:20 +0100

    feat(bin): `dump` switches mode based on rocks-db feature

--- src/bin/dump.rs
+++ src/bin/dump.rs
@@ -1,6 +1,24 @@
 use std::env;
 use std::error::Error;
+#[cfg(feature = "rocks-db")]
+use vecdump::rocks::serialize_to_rocksdb;
 use vecdump::word2vec::serialize_to_store;
+
+/// Dump .txt to .db
+#[cfg(feature = "rocks-db")]
+fn main() -> Result<(), Box<Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("USAGE: dump [PATH]");
+        return Ok(());
+    }
+    let path = args[1].clone();
+    println!("Loading {}...", path);
+    let db = serialize_to_rocksdb(&path)?;
+    println!("Wrote db to {:?}", db);
+    println!("Done!");
+    Ok(())
+}
 
 /// Dump .txt to (.idx, .vec)
 fn main() -> Result<(), Box<Error>> {
